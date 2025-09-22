3dd7eea04793aa4ac75021e4461030ba618dc444
LOVISH-NAGPAL <lovishnagpal0001@gmail.com>
2025-09-22 07:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function stringChop(str, size) {
-  // your code here
+let arr=[]
+	let str1=""
+	
+  for(let i=0;i<str.length;i++){
+	  if(str1.length<size){
+		  str1+=str[i]
+		  
+		 
+		  continue
+	  }
+	  
+	  arr.push(str1)
+	  str1=str[i]
+	  
+  }
+	if(str1.length>0){arr.push(str1)}
+	return arr
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
